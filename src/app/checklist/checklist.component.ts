c91b56c8681e34dfdaaa726761b2f7e25d251aba
Jim Li <jimlizakelijk@outlook.com>
2024-07-04 13:43:22 +0200

    add checklist component

---
+++ src/app/checklist/checklist.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checklist',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      checklist works!
+    </p>
+  `,
+  styles: ``
+})
+export default class ChecklistComponent {
+
+}
